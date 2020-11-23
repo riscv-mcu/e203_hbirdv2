c86916276cff7cb76d57f64c7f6438f79ea38b7b
canhu <hucan0216@163.com>
2020-11-23 16:28:29 +0800

    doc: update HBirdv2 Doc version

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,10 +21,10 @@ copyright = '2020-Present, Nuclei'
 author = 'Nuclei'
 
 # The short X.Y version
-version = '0.1.1'
+version = '0.1.2'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.2'
 
 
 # -- General configuration ---------------------------------------------------
